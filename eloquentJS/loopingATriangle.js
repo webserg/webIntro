ebf8b1b1530d53843d140e610005d10983df7197
webserg@gmail.com <y5OkQ=L8>
2018-08-04 14:04:38 +0300

    eloquent JS

---
+++ eloquentJS/loopingATriangle.js
@@ -0,0 +1,7 @@
+let s = "#";
+for (i = 0; i < 5; i++) {
+    console.log(s)
+    for (j = i; j < i + 1; j++) {
+        s += "#"
+    }
+}
---
+++ hello.js
@@ -0,0 +1,6 @@
+/**
+ * Created by webserg on 5/10/2016.
+ */
+"hello".charAt(0); // "h"
+"hello, world".replace("hello", "goodbye"); // "goodbye, world"
+console.debug("hello".toUpperCase()); // "HELLO"
